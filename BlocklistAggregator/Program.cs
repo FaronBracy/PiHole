f8c4f44fa708de292841941aadb31cf7510ea945
Faron Bracy <signup@dreamersdesign.com>
2024-10-13 17:05:30 -0400

    Blocklist de-duplication

--- BlocklistAggregator/Program.cs
+++ BlocklistAggregator/Program.cs
@@ -4,6 +4,34 @@ namespace BlocklistAggregator;
 
 public class Program
 {
+   public static async Task Main( string[] args )
+   {
+      Blocklist blocklist = new Blocklist();
+
+      string url = "https://v.firebog.net/hosts/Easyprivacy.txt";
+      Console.WriteLine( $"{PrecisionTime()} - Downloading {url}" );
+      string content = await DownloadTextFileAsync( url );
+      Console.WriteLine( $"{PrecisionTime()} - Stripping Content" );
+      string strippedContent = StripIPsAndComments( content );
+      Console.WriteLine( $"{PrecisionTime()} - Removing Duplicates" );
+      AddBlockListResult result = blocklist.AddList( strippedContent );
+      Console.WriteLine( $"{PrecisionTime()} - {result}" );
+
+      url = "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt";
+      Console.WriteLine( $"{PrecisionTime()} - Downloading {url}" );
+      content = await DownloadTextFileAsync( url );
+      Console.WriteLine( $"{PrecisionTime()} - Stripping Content" );
+      strippedContent = StripIPsAndComments( content );
+      Console.WriteLine( $"{PrecisionTime()} - Removing Duplicates" );
+      blocklist.AddList( strippedContent );
+      Console.WriteLine( $"{PrecisionTime()} - {result}" );
+   }
+
+   public static string PrecisionTime()
+   {
+      return $"{DateTime.Now:HH:mm:ss.fffffff}";
+   }
+
    public static async Task<string> DownloadTextFileAsync( string url )
    {
       using HttpClient client = new HttpClient();
@@ -33,13 +61,51 @@ public class Program
 
       return result;
    }
+}
 
-   public static async Task Main( string[] args )
+public class Blocklist
+{
+   private readonly HashSet<string> _uniqueBlocklist = new HashSet<string>();
+
+   public AddBlockListResult AddList( string content )
    {
-      //string url = "https://v.firebog.net/hosts/Easyprivacy.txt";
-      string url = "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt";
-      string content = await DownloadTextFileAsync( url );
-      string strippedContent = StripIPsAndComments( content );
-      Console.WriteLine( strippedContent );
+      int itemsAdded = 0;
+      int duplicateItems = 0;
+
+      string[] lines = content.Split( '\n' );
+      foreach ( string line in lines )
+      {
+         if ( string.IsNullOrWhiteSpace( line ) )
+         {
+            continue;
+         }
+
+         bool result = _uniqueBlocklist.Add( line.Trim() );
+         if ( result )
+         {
+            itemsAdded++;
+         }
+         else
+         {
+            duplicateItems++;
+         }
+      }
+
+      return new AddBlockListResult
+      {
+         ItemsAdded = itemsAdded,
+         DuplicateItems = duplicateItems
+      };
+   }
+}
+
+public class AddBlockListResult
+{
+   public int ItemsAdded { get; set; }
+   public int DuplicateItems { get; set; }
+
+   public override string ToString()
+   {
+      return $"{nameof( ItemsAdded )}: {ItemsAdded}, {nameof( DuplicateItems )}: {DuplicateItems}";
    }
 }
