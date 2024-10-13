a7af1ef2edb90427f1ed9c398a8d34a3ac3c5c87
Faron Bracy <signup@dreamersdesign.com>
2024-10-13 16:39:28 -0400

    Strip IP addresses and whitespace

--- BlocklistAggregator/Program.cs
+++ BlocklistAggregator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BlocklistAggregator;
+﻿using System.Text.RegularExpressions;
+
+namespace BlocklistAggregator;
 
 public class Program
 {
@@ -19,10 +21,20 @@ public class Program
       }
    }
 
+   public static string StripIPs( string content )
+   {
+      // Use a regular expression to remove IP addresses and leading whitespace
+      string pattern = @"^\s*\d{1,3}(\.\d{1,3}){3}\s+";
+      string result = Regex.Replace( content, pattern, "", RegexOptions.Multiline );
+      return result;
+   }
+
    public static async Task Main( string[] args )
    {
-      string url = "https://v.firebog.net/hosts/Easyprivacy.txt";
+      //string url = "https://v.firebog.net/hosts/Easyprivacy.txt";
+      string url = "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt";
       string content = await DownloadTextFileAsync( url );
-      Console.WriteLine( content );
+      string strippedContent = StripIPs( content );
+      Console.WriteLine( strippedContent );
    }
 }
